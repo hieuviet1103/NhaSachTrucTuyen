5fe91a26a265a6d15d49af3bfb0c4c87299562f5
hieuviet1103 <hieuviet.1103@gmail.com>
2020-11-06 16:43:02 +0700

    create new

---
+++ WebApplication3/Areas/DieuHanh/Controllers/LoginController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Threading.Tasks;
+using WebApplication3.Models;
+
+namespace WebApplication3.Areas.DieuHanh.Controllers
+{
+    public class LoginController : Controller
+    {
+        // GET: DieuHanh/Login
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Login(string returnUrl)
+        {
+            ViewBag.ReturnUrl = returnUrl;
+            return View();
+        }
+
+        [HttpPost]
+        [AllowAnonymous]
+        public async Task<ActionResult> Login(LoginViewModel model, string returnUrl)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(model);
+            }
+
+            return View("Index",model);
+        }
+    }
+}
---
+++ WebApplication3/Areas/DieuHanh/DieuHanhAreaRegistration.cs
@@ -0,0 +1,24 @@
+﻿using System.Web.Mvc;
+
+namespace WebApplication3.Areas.DieuHanh
+{
+    public class DieuHanhAreaRegistration : AreaRegistration 
+    {
+        public override string AreaName 
+        {
+            get 
+            {
+                return "DieuHanh";
+            }
+        }
+
+        public override void RegisterArea(AreaRegistrationContext context) 
+        {
+            context.MapRoute(
+                "DieuHanh_default",
+                "DieuHanh/{controller}/{action}/{id}",
+                new { action = "Login", id = UrlParameter.Optional }
+            );
+        }
+    }
+}
